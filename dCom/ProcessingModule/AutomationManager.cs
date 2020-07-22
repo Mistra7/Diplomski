b1cb02603a6195b8d397b086c6448fa896fa513d
Mistra7 <58290564+Mistra7@users.noreply.github.com>
2020-07-22 17:33:41 +0200

    Inicijalni komit

---
+++ dCom/ProcessingModule/AutomationManager.cs
@@ -0,0 +1,131 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ProcessingModule
+{
+    /// <summary>
+    /// Class containing logic for automated work.
+    /// </summary>
+    public class AutomationManager : IAutomationManager, IDisposable
+	{
+		private Thread automationWorker;
+        private AutoResetEvent automationTrigger;
+        private IStorage storage;
+		private IProcessingManager processingManager;
+		private int delayBetweenCommands;
+        private IConfiguration configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutomationManager"/> class.
+        /// </summary>
+        /// <param name="storage">The storage.</param>
+        /// <param name="processingManager">The processing manager.</param>
+        /// <param name="automationTrigger">The automation trigger.</param>
+        /// <param name="configuration">The configuration.</param>
+        public AutomationManager(IStorage storage, IProcessingManager processingManager, AutoResetEvent automationTrigger, IConfiguration configuration)
+		{
+			this.storage = storage;
+			this.processingManager = processingManager;
+            this.configuration = configuration;
+            this.automationTrigger = automationTrigger;
+        }
+
+        /// <summary>
+        /// Initializes and starts the threads.
+        /// </summary>
+		private void InitializeAndStartThreads()
+		{
+			InitializeAutomationWorkerThread();
+			StartAutomationWorkerThread();
+		}
+
+        /// <summary>
+        /// Initializes the automation worker thread.
+        /// </summary>
+		private void InitializeAutomationWorkerThread()
+		{
+			automationWorker = new Thread(AutomationWorker_DoWork);
+			automationWorker.Name = "Aumation Thread";
+		}
+
+        /// <summary>
+        /// Starts the automation worker thread.
+        /// </summary>
+		private void StartAutomationWorkerThread()
+		{
+			automationWorker.Start();
+		}
+
+
+		private void AutomationWorker_DoWork()
+		{
+			List<PointIdentifier> points = new List<PointIdentifier>()
+			{
+				new PointIdentifier(PointType.DIGITAL_OUTPUT, 1100),
+				new PointIdentifier(PointType.DIGITAL_INPUT, 1000),
+				new PointIdentifier(PointType.ANALOG_INPUT, 2000),
+				new PointIdentifier(PointType.ANALOG_OUTPUT, 3000)
+			};
+
+			var realPoints = storage.GetPoints(points);
+
+			while (!disposedValue)
+			{
+				foreach(IPoint p in realPoints)
+				{
+					++p.ConfigItem.SecondsPassedSinceLastPoll;
+					if (p.ConfigItem.SecondsPassedSinceLastPoll == p.ConfigItem.AcquisitionInterval)
+					{
+						processingManager.ExecuteReadCommand(p.ConfigItem, configuration.GetTransactionId(), configuration.UnitAddress, p.ConfigItem.StartAddress, p.ConfigItem.NumberOfRegisters);
+						p.ConfigItem.SecondsPassedSinceLastPoll = 0;
+					}					
+				}
+
+				automationTrigger.WaitOne();
+			}
+		}
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+
+        /// <summary>
+        /// Disposes the object.
+        /// </summary>
+        /// <param name="disposing">Indication if managed objects should be disposed.</param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+				}
+				disposedValue = true;
+			}
+		}
+
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// GC.SuppressFinalize(this);
+		}
+
+        /// <inheritdoc />
+        public void Start(int delayBetweenCommands)
+		{
+            InitializeAndStartThreads();
+		}
+
+        /// <inheritdoc />
+        public void Stop()
+		{
+			Dispose();
+		}
+		#endregion
+	}
+}
